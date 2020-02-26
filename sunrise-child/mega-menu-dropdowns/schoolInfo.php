b98a6476e109f5b66c795ba8d238829654e34eea
Josh Espinoza <joshespi@gmial.com>
2020-02-26 07:38:59 -0700

    updated theme address so its not broken, removed school meals stuff since preschool doesn't have a lunch program, updated safe schools link

--- sunrise-child/mega-menu-dropdowns/schoolInfo.php
+++ sunrise-child/mega-menu-dropdowns/schoolInfo.php
@@ -25,16 +25,5 @@
 		<ul>
 			<li class="ext"><a href="https://grades.provo.edu/public/">Grades & Attendance (PowerSchool)</a></li>
 			<li class="int"><a href="https://sunrise.provo.edu/faculty-staff/teachers-directory/">Classrooms</a></li>
-			<li class="ext"><a href="http://www.preschool.provo.edu//Site_Home/registration-2018-2019.html">Pay School Fees</a></li>
 		</ul>
-	<!--
-	<h3><img src="https://sunrise.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-meals.svg" alt="" />School Meals</h3>
-		<ul>
-			<li class="ext"><a href="https://www.myschoolbucks.com/ver2/login/getmain?requestAction=home">Pay for School Meals</a></li>
-            <li class="ext"><a href="https://www.schoollunchapp.com/Terms.aspx?host=provo.schoollunchapp.com&apply=1">Apply for Free and Reduced Meals</a></li>
-            <li class="ext"><a href="http://itsmealsprovo.com/?page=menus">School Menus</a></li>
-            <li class="int"><a href="https://provo.edu/child-nutrition/special-diet-accommodations/">Special Diet Accommodations</a></li>
-            <li class="int"><a href="https://provo.edu/child-nutrition/">Provo Child Nutrition Website</a></li>
-		</ul>
-	-->
 </div>
